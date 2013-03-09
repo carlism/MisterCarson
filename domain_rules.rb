a5f3cc56852aeff30356d57b1a044715b9a33e43
Carl Leiby <carl.leiby@gmail.com>
2013-03-08 21:41:38 -0500

    fixed value match

--- domain_rules.rb
+++ domain_rules.rb
@@ -47,8 +47,7 @@ rule :switch_floods_with_door do
         if id.zw_node_key == @nodes["Side Door Light"]
             state = store[id.zw_node_key][:v_Basic].value
             floods = ZwaveId.new(@nodes["Flood Lights"])
-            @log.debug "matched node, state #{state.inspect}"
-            if state == "1"
+            if state.to_i > 0
                 publish :zw_turn_on_node, "#{floods.home}:#{floods.node}"
             else
                 publish :zw_turn_off_node, "#{floods.home}:#{floods.node}"
